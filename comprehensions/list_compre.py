462a4fcdabdee96d4af2a2336a5ee42e9a304bb7
Divyansh Sharma <divyanshsharma@Divyanshs-MacBook-Air.local>
2026-03-23 22:33:34 +0530

    comprehension

---
+++ comprehensions/list_compre.py
@@ -0,0 +1,6 @@
+my_list = ["apples","banana","mangoes","strawberry"]
+
+# expression for item in iterable if condition
+fav_fruite = [fruit for fruit in my_list if len(fruit) > 9]
+
+print(fav_fruite)
